251084ff8fa4d8b5516ba6f26f75e6745a86b5ee
mairess <trabalho.maires@gmail.com>
2024-05-20 19:25:46 -0300

    feat: implements GET /closest

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,14 +1,19 @@
 package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
+import jdk.jfr.Name;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +47,27 @@ public class MuseumController {
   public Museum createMuseum(@RequestBody MuseumCreationDto newMuseum) {
     return service.createMuseum(ModelDtoConverter.dtoToModel(newMuseum));
   }
+
+  /**
+   * Gets closest museum.
+   *
+   * @param lat       the lat
+   * @param lng       the lng
+   * @param maxDistKm the max dist km
+   * @return the closest museum
+   */
+  @GetMapping("/closest")
+  public MuseumDto getClosestMuseum(
+      @RequestParam double lat,
+      @RequestParam double lng,
+      @RequestParam("max_dist_km") double maxDistKm
+  ) {
+    Coordinate coordinate = new Coordinate(lat, lng);
+
+    Museum closestMuseum = service.getClosestMuseum(coordinate, maxDistKm);
+
+    return ModelDtoConverter.modelToDto(closestMuseum);
+
+  }
+
 }
