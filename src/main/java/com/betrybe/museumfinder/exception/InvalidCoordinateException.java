9f10fac388f6fb5958ba38c51f86584cd32851e4
mairess <trabalho.maires@gmail.com>
2024-05-20 10:10:29 -0300

    feat: implements method createMuseum in class MuseumService and add exception InvalidCoordinateException

---
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -0,0 +1,15 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * The type Invalid coordinate exception.
+ */
+public class InvalidCoordinateException extends RuntimeException {
+
+  /**
+   * Instantiates a new Invalid coordinate exception.
+   */
+  public InvalidCoordinateException() {
+
+  }
+
+}
---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,56 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * The type Museum service.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+
+  private MuseumFakeDatabase museumFakeDatabase;
+
+  /**
+   * Instantiates a new Museum service.
+   *
+   * @param museumFakeDatabase the museum fake database
+   */
+  @Autowired
+  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    return null;
+  }
+
+  @Override
+  public Museum createMuseum(Museum museum) {
+
+    Coordinate coordinate = museum.getCoordinate();
+
+    if (CoordinateUtil.isCoordinateValid(coordinate)) {
+
+      return museumFakeDatabase.saveMuseum(museum);
+
+    } else {
+
+      throw new InvalidCoordinateException();
+
+    }
+
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
